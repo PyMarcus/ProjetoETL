e50856a813059153ad0e07e8240b5de4907e6a19
Marcus Vinícius <marcus-v@outlook.com>
2021-09-02 01:07:26 -0300

    up

---
+++ p.py
@@ -0,0 +1,14 @@
+with open('ocorrencia.csv', 'r') as f:
+    arquivo = f.readlines()
+    with open('ocorrencia2.csv', 'w') as f:
+        for linha in arquivo:
+            if linha.split(';')[9].startswith('*'):
+                print(linha.index('***'))
+                arquivo = f.writelines(linha.replace('***', ''))
+            else:
+                arquivo = f.writelines(linha)
+with open('ocorrencia2.csv', 'r') as f:
+    arquivo = f.readlines()
+    for linhas in arquivo:
+        if linha.split(';')[9].startswith('*'):
+                print(linha.index('***'))
